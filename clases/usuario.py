6d89ce2e5a6a5f84b0f008956c7b34532e24ff0b
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-08 18:57:12 -0300

    feat: Añadí el atributo "nombre_usuario" y su set y get en usuario.py

--- clases/usuario.py
+++ clases/usuario.py
@@ -1,12 +1,15 @@
 from clases.ficha import Ficha
 
 class Usuario(Ficha):
-    def __init__(self, id_ficha, nombre, apellido, dni, cuil, telefono, mail, fecha_nacimiento, direccion, contrasena, rol):
+    def __init__(self, id_ficha, nombre, apellido, dni, cuil, telefono, mail, fecha_nacimiento, direccion, nombre_usuario, contrasena, rol):
         super().__init__(id_ficha, nombre, apellido, dni, cuil, telefono, mail, fecha_nacimiento, direccion)
+        self.nombre_usuario = nombre_usuario
         self.contrasena = contrasena
         self.rol = rol
 
     # Getters
+    def get_nombre_usuario(self):
+        return self.nombre_usuario
     def get_contrasena(self):
         return self.contrasena
 
@@ -14,6 +17,9 @@ class Usuario(Ficha):
         return self.rol
 
     # Setters
+    def set_nombre_usuario(self, nombre_usuario):
+        self.nombre_usuario = nombre_usuario
+
     def set_contrasena(self, contrasena):
         self.contrasena = contrasena
 
