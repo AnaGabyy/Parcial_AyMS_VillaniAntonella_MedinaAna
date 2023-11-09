b3c27b86498f8854a0d571b9eb5f0293114fcf58
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-08 21:05:54 -0300

    feat: Añadí el método para mostrar la interfaz de inicio_sesion y la prueba unitaria en main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import inicio_sesion
+from clases.inicio_sesion import Inicio_Sesion
+#from test.test_inicio_sesion import Test_Inicio_Sesion
 
 # Mostrar interfaz de inicio de sesión
-inicio_sesion.mostrar_interfaz_inicio_sesion()
+interfaz_inicio_sesion = Inicio_Sesion()
+interfaz_inicio_sesion.mostrar_interfaz_inicio_sesion()
+
+# Ejecutar prueba unitaria
+#test_inicio_sesion = Test_Inicio_Sesion()
+#test_inicio_sesion.test_verificar_usuario()
